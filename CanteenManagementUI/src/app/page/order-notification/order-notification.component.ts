e01b8c4e01369267a831954a91c4bb7eda19c22f
Himanshu Rajput <himanshu.rajput@rdias.ac.in>
2025-10-14 18:29:43 +0530

    notificationtones

---
+++ CanteenManagementUI/src/app/page/order-notification/order-notification.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-notification',
+  standalone: true,
+  imports: [],
+  templateUrl: './order-notification.component.html',
+  styleUrl: './order-notification.component.scss'
+})
+export class OrderNotificationComponent {
+
+}
