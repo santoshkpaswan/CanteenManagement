e307779c4e587a2447b12e53f86a57147af14c59
Santosh <santoshkpaswan@gmail.com>
2025-11-15 18:24:04 +0530

    commit after change in environement

--- CanteenManagementUI/src/environments/environment.prod.ts
+++ CanteenManagementUI/src/environments/environment.prod.ts
@@ -5,6 +5,6 @@ export const environment = {
   production: true,
   apiUrl : 'http://192.168.0.252:5001/api', // 252 //10 SERVER LIVE
   imageUrl : 'http://192.168.0.252:5001/files/', // 252 //10 SERVER LIVE
-  apiLoginUrl: 'http://eshaala.rdias.ac.in:89/API',
-  _apiLoginUrl:'http://eshaala.rdias.ac.in:86/API'
+  _apiLoginUrl: 'http://eshaala.rdias.ac.in:89/API', //development
+  apiLoginUrl:'http://eshaala.rdias.ac.in:86/API' //Live
 };
